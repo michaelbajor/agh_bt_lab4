1c7fc5e1c534223f736fc43e4ddd589ce3503f43
michaelbajor <36539787+michaelbajor@users.noreply.github.com>
2025-04-12 15:47:33 +0200

    PaymentSystem contract

---
+++ src/PaymentSystem.sol
@@ -0,0 +1,56 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+contract PaymentSystem {
+    event PaymentCreated(
+        address indexed creator,
+        address indexed receiver,
+        uint256 amount
+    );
+    event PaymentClaimed(address indexed claimer, uint256 amount);
+
+    struct ClaimablePayment {
+        bool exists; // is that required? If so, why? :)
+        uint amount;
+        string description;
+    }
+
+    mapping(address => ClaimablePayment) public _payments;
+
+    constructor() {}
+
+    function getPaymentFor(
+        address recipient
+    ) external view returns (ClaimablePayment memory) {
+        return _payments[recipient];
+    }
+
+    function createPayment(
+        address recipient,
+        string memory description
+    ) external payable {
+        require(!_payments[recipient].exists, "Unclaimed payment exists");
+        require(msg.value > 0, "Zero payment");
+        require(bytes(description).length <= 255, "Description too long");
+
+        ClaimablePayment memory payment = ClaimablePayment(
+            true,
+            msg.value,
+            description
+        );
+        _payments[recipient] = payment;
+
+        emit PaymentCreated(msg.sender, recipient, msg.value);
+    }
+
+    function claimPayment() external {
+        ClaimablePayment storage payment = _payments[msg.sender];
+        require(payment.exists, "No available payment");
+
+        (bool success, ) = payable(msg.sender).call{value: payment.amount}("");
+        require(success, "Transfer failed");
+
+        payment.exists = false;
+        emit PaymentClaimed(msg.sender, payment.amount);
+    }
+}
---
+++ test/PaymentSystem.t.sol
@@ -0,0 +1,56 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console} from "forge-std/Test.sol";
+import {PaymentSystem} from "../src/PaymentSystem.sol";
+
+contract CounterTest is Test {
+    PaymentSystem public paymentSystem;
+    address public user1 = address(1);
+    address public user2 = address(2);
+
+    function setUp() public {
+        paymentSystem = new PaymentSystem();
+    }
+
+    function test_paymentWorks() public {
+        // user1 creates payment
+        string memory description = "Payment for invoice 1/1/2025";
+        vm.deal(user1, 10 ether);
+        vm.prank(user1);
+        paymentSystem.createPayment{value: 1 ether}(user2, description);
+
+        // sanity check - Payment was created
+        PaymentSystem.ClaimablePayment memory payment = paymentSystem
+            .getPaymentFor(user2);
+        assertTrue(payment.exists);
+        assertEq(payment.amount, 1 ether);
+        assertEq(payment.description, description);
+
+        // it should be impossible to create second payment for user2
+        string memory description2 = "Doesn't matter";
+        vm.prank(user1);
+        vm.expectRevert("Unclaimed payment exists");
+        paymentSystem.createPayment{value: 2 ether}(user2, description2);
+
+        // user2 claims payment
+        uint256 user2BalanceBefore = user2.balance;
+        console.logUint(user2BalanceBefore); // if you want to see it in console.log, run with -vv
+
+        vm.prank(user2);
+        paymentSystem.claimPayment();
+
+        uint256 user2BalanceAfter = user2.balance;
+        console.logUint(user2BalanceAfter);
+        assertEq(user2BalanceBefore + 1 ether, user2BalanceAfter);
+
+        // user2 shouldn't be able to claim again
+        vm.prank(user2);
+        vm.expectRevert("No available payment");
+        paymentSystem.claimPayment();
+    }
+
+    function test_exploit() public {
+        // TODO
+    }
+}
